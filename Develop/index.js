f4a3e2b7f39c27532633756f1bb693ded1caa621
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-05-01 15:21:40 -0400

    Updated index.js with questions and readme template

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,123 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type:"input",
+        name: "projectTitle",
+        message: "What is the title of your project?"
+    }, {
+        type:"input",
+        name: "description",
+        message: "Write a short description explaining the what, why and hows of your project."
+    }, {
+        type:"input",
+        name: "installation",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+    }, {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use."
+    }, {
+        type:"input",
+        name: "contributing",
+        message: "List your collaborators, if any, with links to their GitHub profiles."
+    }, {
+        type:"input",
+        name:"tests",
+        message:"List test instructions if needed."
+    }, {
+        type:"input",
+        name:"license",
+        message:"license"
+    }, {
+        type:"input",
+        name:"gitUsername",
+        message:"Enter your GitHub username."
+    }, {
+        type:"input",
+        name:"email",
+        message:"Enter your email."
+    }
+]).then(result=>{
+    const {projectTitle,description,installation, usage, contributing, tests, license, gitUsername, email}=result;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    const readme = 
+`# ${projectTitle}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${description}
 
-// Function call to initialize app
-init();
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#tests)
+
+## Installation 
+${installation}
+
+## Usage 
+${usage}
+
+## License 
+${license}
+
+## Contributing 
+${contributing}
+
+## Tests 
+${tests}
+
+## Questions 
+For any further questions, email me at:${email}. GitHub:github.com/${gitUsername}, `
+
+
+    fs.writeFile("README.md",readme, function(error){
+        if(error){
+            console.log("There was an error with your README file.")
+        } else {
+            console.log("The README file was saved successfully!")
+
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // TODO: Include packages needed for this application
+// const inquirer = require("inquirer");
+// const fs = require("fs");
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName,data,function(error){
+//         if(error){
+//             console.log("There was an error saving your README file.")
+//         } else {
+//             console.log("Your README file was saved successfully!")
+//         }
+//     })
+// }
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
